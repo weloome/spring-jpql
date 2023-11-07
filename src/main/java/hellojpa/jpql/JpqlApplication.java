f7bcdcd5bf8bee9e057669166e7351fe5730e16b
danbi <leedanbiii@naver.com>
2023-11-07 14:09:41 +0900

    엔티티 생성

--- src/main/java/hellojpa/jpql/JpqlApplication.java
+++ src/main/java/hellojpa/jpql/JpqlApplication.java
@@ -1,5 +1,9 @@
 package hellojpa.jpql;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +11,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpqlApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JpqlApplication.class, args);
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+
+            Member member = new Member();
+            member.setUsername("member1");
+            em.persist(member);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
     }
 
 }
