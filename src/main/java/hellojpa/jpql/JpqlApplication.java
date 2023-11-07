e77001a7d2024f358de1529685393fc8b3bcd8d2
danbi <leedanbiii@naver.com>
2023-11-07 16:54:24 +0900

    조건식 - 기본 CASE 식

--- src/main/java/hellojpa/jpql/JpqlApplication.java
+++ src/main/java/hellojpa/jpql/JpqlApplication.java
@@ -31,11 +31,19 @@ public class JpqlApplication {
             em.flush();
             em.clear();
 
-            String query = "select m from Member m, Team t where m.username = t.name";
-            List<Member> result = em.createQuery(query, Member.class)
-                    .getResultList();
-            System.out.println("result = " + result);
+            String query = "select " +
+                                "case when m.age <= 10 then '학생요금'" +
+                                "     when m.age >= 60 then '경로요금'" +
+                                "     else '일반요금' " +
+                                "end " + 
+                            " from Member m";
+            List<String> result = em.createQuery(query, String.class).getResultList();
             
+            for (String s : result) {
+                System.out.println("s = " + s);
+            }
+            
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
