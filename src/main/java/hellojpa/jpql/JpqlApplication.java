7b017e8b187b4b4a73393750c9dd37e7fa254b04
danbi <leedanbiii@naver.com>
2023-11-08 02:06:57 +0900

    벌크 연산

--- src/main/java/hellojpa/jpql/JpqlApplication.java
+++ src/main/java/hellojpa/jpql/JpqlApplication.java
@@ -28,29 +28,32 @@ public class JpqlApplication {
 
             Member member1 = new Member();
             member1.setUsername("회원1");
+            member1.setAge(0);
             member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
             member2.setUsername("회원2");
+            member2.setAge(0);
             member2.setTeam(teamA);
             em.persist(member2);
 
             Member member3 = new Member();
             member3.setUsername("회원3");
+            member3.setAge(0);
             member3.setTeam(teamB);
             em.persist(member3);
 
             em.flush();
             em.clear();
 
-            List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class)
-                            .setParameter("username", "회원1")
-                                    .getResultList();
-            
-            for (Member member : resultList) {
-                System.out.println("member = " + member);
-            }
+            // 모든 회원의 나이를 20살로 바꿔
+            int resultCount = em.createQuery("update Member m set m.age = 20")
+                            .executeUpdate();
+
+            em.clear();
+            Member findMember = em.find(Member.class, member1.getId());
+            System.out.println("findMember = " + findMember.getAge());
 
             tx.commit();
         } catch (Exception e) {
