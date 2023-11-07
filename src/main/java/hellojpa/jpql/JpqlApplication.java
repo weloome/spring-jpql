7433916faa3d4266b7070d968d8270cf6d11008d
danbi <leedanbiii@naver.com>
2023-11-07 15:49:30 +0900

    조인 - 내부

--- src/main/java/hellojpa/jpql/JpqlApplication.java
+++ src/main/java/hellojpa/jpql/JpqlApplication.java
@@ -18,25 +18,25 @@ public class JpqlApplication {
 
         try {
 
-            for (int i=0; i<100; i++) {
-                Member member = new Member();
-                member.setUsername("member" + i);
-                member.setAge(i);
-                em.persist(member);
-            }
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            member.setTeam(team);
+            em.persist(member);
 
             em.flush();
             em.clear();
 
-            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(10)
+            String query = "select m from Member m inner join m.team t where t.name = :teamName";
+            List<Member> result = em.createQuery(query, Member.class)
+                    .setParameter("teamName", "teamA")
                     .getResultList();
-            System.out.println("result.size = " + result.size());
-            for (Member member1 : result) {
-                System.out.println("member1 = " + member1);
-            }
-
+            System.out.println("result = " + result);
+            
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- src/main/java/hellojpa/jpql/Member.java
+++ src/main/java/hellojpa/jpql/Member.java
@@ -9,9 +9,14 @@ public class Member {
     private String username;
     private int age;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "TEAM_ID")
     private Team team;
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
 
     public Long getId() {
         return id;
@@ -35,6 +40,14 @@ public class Member {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
     }
 
     @Override
