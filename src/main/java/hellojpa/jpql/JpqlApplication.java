6cc69e23461b628370107a3f56ad818c75aac834
danbi <leedanbiii@naver.com>
2023-11-07 17:00:04 +0900

    조건식 - NULLIF

--- src/main/java/hellojpa/jpql/JpqlApplication.java
+++ src/main/java/hellojpa/jpql/JpqlApplication.java
@@ -23,7 +23,7 @@ public class JpqlApplication {
             em.persist(team);
 
             Member member = new Member();
-            member.setUsername(null);
+            member.setUsername("관리자");
             member.setAge(10);
             member.setTeam(team);
             em.persist(member);
@@ -31,7 +31,7 @@ public class JpqlApplication {
             em.flush();
             em.clear();
 
-            String query = "select coalesce(m.username, '이름 없는 회원') from Member m";
+            String query = "select nullif(m.username, '관리자') from Member m";
             List<String> result = em.createQuery(query, String.class).getResultList();
             
             for (String s : result) {
